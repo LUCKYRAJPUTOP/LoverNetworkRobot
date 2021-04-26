71ea2c3d41ef95405e141c9fda1af7ff3f1971f4
Anshul Rajput <82425167+LUCKYRAJPUTOP@users.noreply.github.com>
2021-04-26 16:51:42 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE PglRobot
-:: Enables virtual env mode and then starts PglRobot
-env\scripts\activate.bat && py -m PglRobot
+TITLE LoverNetworkRobot
+:: Enables virtual env mode and then starts LoverNetworkRobot
+env\scripts\activate.bat && py -m LoverNetworkRobot
